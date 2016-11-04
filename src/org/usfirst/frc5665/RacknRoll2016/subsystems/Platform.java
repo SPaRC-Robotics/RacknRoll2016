5744c901dcd6b7b3c62b3c67e2f43ad76666c32a
Cagatay Yigit Sahin <sahcag.20@robcol.k12.tr>
2016-11-04 19:00:24 +0200

    Added platform subsystem and defined unlock command

---
+++ src/org/usfirst/frc5665/RacknRoll2016/subsystems/Platform.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc5665.RacknRoll2016.subsystems;
+
+import org.usfirst.frc5665.RacknRoll2016.RobotMap;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Platform extends Subsystem {
+    
+    private final boolean isLocked = true;
+    private final Relay lock = RobotMap.platformLock;
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void unlock() {
+        lock.set(Relay.Value.kOn);
+    }
+}
+
+
